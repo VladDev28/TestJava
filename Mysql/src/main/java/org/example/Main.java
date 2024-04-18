d27c685dbe6f01e56289f18baa7c103f0c395c4e
VladDev <vlad.iachim15@gmail.com>
2024-04-18 11:41:51 +0300

    small changes

--- Mysql/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    String url = "jdbc:mysql://localhost:3306/test";
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
